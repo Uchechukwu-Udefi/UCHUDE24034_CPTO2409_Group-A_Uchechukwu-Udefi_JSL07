3f7d0acdeea05269ae7b3328825a3a43d564b043
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-02-26 09:32:15 +0200

    Get input value elements by id

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName');
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
   });
   
